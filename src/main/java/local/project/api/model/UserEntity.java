a47a17f16c09e9c109077d9cc205723903f8b600
tetris8010 <tetris8010@gmail.com>
2020-03-18 15:47:12 +0900

    UserEntity getter, setter, MtoM cascade 추가

--- src/main/java/local/project/api/model/UserEntity.java
+++ src/main/java/local/project/api/model/UserEntity.java
@@ -1,15 +1,20 @@
 package local.project.api.model;
 
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 @Table(name = "user")
 
 public class UserEntity extends DefaultEntity {	
+
 	@Column(unique=true, nullable=false)
 	private String loginid;
 
@@ -29,59 +34,106 @@ public class UserEntity extends DefaultEntity {
 	@Column
 	private String phone;
 
+	@ManyToMany(cascade = CascadeType.PERSIST)
+	private List<UserEntity> friend;
 
 
+	
+	/**
+	 * @return the loginid
+	 */
 	public String getLoginid() {
 		return loginid;
 	}
 
+	/**
+	 * @param loginid the loginid to set
+	 */
 	public void setLoginid(String loginid) {
 		this.loginid = loginid;
 	}
-
-
+	
+	/**
+	 * @return the username
+	 */
 	public String getUsername() {
 		return username;
-	}	
+	}
 
+	/**
+	 * @param username the username to set
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-
+	/**
+	 * @return the password
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * @param password the password to set
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-
+	/**
+	 * @return the sex
+	 */
 	public char getSex() {
 		return sex;
-
 	}
 
-	public void setSex(char sex){
+	/**
+	 * @param sex the sex to set
+	 */
+	public void setSex(char sex) {
 		this.sex = sex;
 	}
 
-
+	/**
+	 * @return the birth
+	 */
 	public String getBirth() {
 		return birth;
 	}
 
-	public void setBirth(String birth){
+	/**
+	 * @param birth the birth to set
+	 */
+	public void setBirth(String birth) {
 		this.birth = birth;
 	}
-
 	
+	/**
+	 * @return the phone
+	 */
 	public String getPhone() {
 		return phone;
 	}
 
+	/**
+	 * @param phone the phone to set
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
+	}
+
+	/**
+	 * @return the friend
+	 */
+	public List<UserEntity> getFriend() {
+		return friend;
+	}
+
+	/**
+	 * @param friend the friend to set
+	 */
+	public void setFriend(List<UserEntity> friend) {
+		this.friend = friend;
 	}
 }
