1999269c44a8e1531b1ba37233a7be77623c750a
Ji2z <leegin98@naver.com>
2020-03-18 11:53:04 +0900

    오류 수정

--- src/main/java/local/project/api/service/UserService.java
+++ src/main/java/local/project/api/service/UserService.java
@@ -5,22 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
-import local.project.api.model.DocumentEntity;
-import local.project.api.model.DocumentRepository;
-
-@Service
-public class UserService extends DefaultService<DocumentEntity> {
-
-    @Autowired
-    private DocumentRepository documentRepository;
-
-    public Page<DocumentEntity> getAll(int page) {
-        return documentRepository.findAllByIsDelFalse(PageRequest.of(page, 20));
-	}
-	
-	
-=======
 import local.project.api.model.UserEntity;
 import local.project.api.model.UserRepository;
 
@@ -33,6 +17,4 @@ public class UserService extends DefaultService<UserEntity> {
     public Page<UserEntity> getAll(int page) {
         return userRepository.findAllByIsDelFalse(PageRequest.of(page, 20));
 	}
-	
->>>>>>> 208343ec5143bfca9ed50a15b975ac208c9703ac
 }
