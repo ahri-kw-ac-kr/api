c265736938030b8d2809f5ab9a22a046e150fd2f
Ji2z <leegin98@naver.com>
2020-05-31 00:31:16 +0900

    sleepTime pk 추가 설정

--- src/main/java/local/project/api/model/SleepEntity.java
+++ src/main/java/local/project/api/model/SleepEntity.java
@@ -5,11 +5,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table(name = "sleep")
+@Table(name = "sleep",uniqueConstraints={@UniqueConstraint(columnNames={"sleepTime","user_id"})})
 @JsonIgnoreProperties({"updatedAt", "del"})
 public class SleepEntity extends DefaultEntity {
 
